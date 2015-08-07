a59a5b3febd9f94b927d82a811ff2f798e115bd2
japlee <japlee1986@gmail.com>
2015-08-07 17:33:41 +0800

    Update test.erl

--- test.erl
+++ test.erl
@@ -6,3 +6,14 @@
 
 print() ->
   io:format("Hello, git!~n").
+
+add(x, y) is_number(x), is_number(y) ->
+  x + y.
+  
+list_sum(List) ->
+  list_sum_help(List, 0).
+
+list_sum_help([], Sum) ->
+  Sum;
+list_sum_help([H|T], Sum) is_number(H) ->
+  list_sum_help(T, Sum + H).
