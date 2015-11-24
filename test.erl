4fe6a770b064e40a887c29c5ca79e03f162b2f3e
japlee <japlee1986@gmail.com>
2015-11-24 17:01:36 +0800

    Update test.erl

--- test.erl
+++ test.erl
@@ -19,7 +19,7 @@ list_sum_help([H|T], Sum) is_number(H) ->
   list_sum_help(T, Sum + H).
 
 q_sort([]) -> [];
-q_sort([Pivot|T]) -> q_sort([E || E < T, E < Pivot]) ++ [Pivot] ++ q_sort([E || E <- T, E >= Pivot]).
+q_sort([Pivot|T]) -> q_sort([E || E <- T, E < Pivot]) ++ [Pivot] ++ q_sort([E || E <- T, E >= Pivot]).
 
 all_queue([]) -> [[]];
 all_queue(L) -> [[H|T] || H <- L, T <- all_queue(L -- [H])].
